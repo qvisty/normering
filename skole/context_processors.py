7629caa0b9f3ea4669a545235bcfc8519b266048
Qvisty <jgq@live.dk>
2024-07-07 11:58:58 +0200

    tilføjet topmenu og context processor

---
+++ skole/context_processors.py
@@ -0,0 +1,28 @@
+from django.db.models import IntegerField, Value
+from django.db.models.functions import Cast
+from django.db.models import Case, When, F
+from .models import School, Department, Team, SchoolClass
+
+
+def global_context(request):
+    all_schools = School.objects.all()
+    all_departments = Department.objects.all().order_by(
+        "name"
+    )  # Sorter afdelinger alfabetisk
+    all_teams = Team.objects.all().order_by(
+        "department__name", "name"
+    )  # Sorter teams efter afdeling og derefter alfabetisk indenfor afdeling
+
+    all_school_classes = SchoolClass.objects.annotate(
+        class_number=Cast(
+            Case(When(name__regex=r"^\d+", then=F("name")), default=Value("0")),
+            IntegerField(),
+        )
+    ).order_by("team__department__name", "class_number", "name")
+
+    return {
+        "all_schools": all_schools,
+        "all_departments": all_departments,
+        "all_teams": all_teams,
+        "all_school_classes": all_school_classes,
+    }
--- skole/models.py
+++ skole/models.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 from django.db import models
 from datetime import datetime
 from django.utils.timesince import timesince
+import re
+
 
 class School(models.Model):
     name = models.CharField(max_length=100)
@@ -9,21 +11,26 @@ class School(models.Model):
     def __str__(self):
         return self.name
 
+
 class Department(models.Model):
     name = models.CharField(max_length=100)
-    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='departments')
-
+    school = models.ForeignKey(
+        School, on_delete=models.CASCADE, related_name="departments"
+    )
 
     def __str__(self):
         return self.name
 
+
 class Team(models.Model):
     name = models.CharField(max_length=100)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='teams')
-
+    department = models.ForeignKey(
+        Department, on_delete=models.CASCADE, related_name="teams"
+    )
 
     def __str__(self):
         return f"{self.department.name} - {self.name}"
+
 
 class EmploymentCategory(models.Model):
     name = models.CharField(max_length=100)
@@ -33,16 +40,17 @@ class EmploymentCategory(models.Model):
     def __str__(self):
         return self.name
 
+
 class Staff(models.Model):
     name = models.CharField(max_length=100)
-    employment_category = models.ForeignKey(EmploymentCategory, on_delete=models.CASCADE, related_name='staff_members')
+    employment_category = models.ForeignKey(
+        EmploymentCategory, on_delete=models.CASCADE, related_name="staff_members"
+    )
     employment_grade = models.IntegerField(default=37)
 
     def __str__(self):
         return self.name
-    
 
-    
     @classmethod
     def total_lessons_taught_in_class(cls, school_class):
         """
@@ -50,24 +58,37 @@ class Staff(models.Model):
         """
         return cls.objects.filter(taught_lessons__school_class=school_class).count()
 
+
 from collections import Counter
 
+
 class SchoolClass(models.Model):
-    IND = 'Indskoling'
-    MEL = 'Mellemtrin'
-    UDS = 'Udskoling'
+    IND = "Indskoling"
+    MEL = "Mellemtrin"
+    UDS = "Udskoling"
     AGE_GROUP_CHOICES = [
-        (IND, 'Indskoling'),
-        (MEL, 'Mellemtrin'),
-        (UDS, 'Udskoling'),
+        (IND, "Indskoling"),
+        (MEL, "Mellemtrin"),
+        (UDS, "Udskoling"),
     ]
     DEFAULT_AGE_GROUP = IND  # Sæt standardværdi til 'Indskoling'
 
     name = models.CharField(max_length=100)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='school_classes')
-    class_teachers = models.ManyToManyField(Staff, related_name='class_teachers', blank=True)
-    class_group = models.CharField(max_length=20, choices=AGE_GROUP_CHOICES, default=DEFAULT_AGE_GROUP)
+    team = models.ForeignKey(
+        Team, on_delete=models.CASCADE, related_name="school_classes"
+    )
+    class_teachers = models.ManyToManyField(
+        Staff, related_name="class_teachers", blank=True
+    )
+    class_group = models.CharField(
+        max_length=20, choices=AGE_GROUP_CHOICES, default=DEFAULT_AGE_GROUP
+    )
     age_number = models.IntegerField(blank=True, null=True)
+
+    def get_class_number(self):
+        # Uddrag den første heltalsværdi fra klassens navn
+        match = re.match(r"(\d+)", self.name)
+        return int(match.group(0)) if match else 0
 
     def total_lessons_per_category(self):
         """
@@ -85,31 +106,40 @@ class SchoolClass(models.Model):
                 category = teacher.employment_category.name
                 # Tilføj antallet af lektioner for denne lektion til den tilsvarende kategori i dictionarien
                 total_lessons_by_category[category] += 1
-            
+
         return total_lessons_by_category
-        
+
     def __str__(self):
         return self.name
+
 
 class StudentInSchoolManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(school_class__team__department__school__isnull=False)
+        return (
+            super()
+            .get_queryset()
+            .filter(school_class__team__department__school__isnull=False)
+        )
 
     def __str__(self):
         return self.name
+
 
 class StudentInSchoolClassManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(school_class__isnull=False)
 
+
 class Lesson(models.Model):
-    school_class = models.ForeignKey(SchoolClass, on_delete=models.CASCADE, related_name='lessons')
-    subject = models.CharField(max_length=100) # fag
-    classroom = models.CharField(max_length=100) #lokale
-    teachers = models.ManyToManyField(Staff, related_name='taught_lessons')
+    school_class = models.ForeignKey(
+        SchoolClass, on_delete=models.CASCADE, related_name="lessons"
+    )
+    subject = models.CharField(max_length=100)  # fag
+    classroom = models.CharField(max_length=100)  # lokale
+    teachers = models.ManyToManyField(Staff, related_name="taught_lessons")
 
     def __str__(self):
-        teachers_names = ', '.join([teacher.name for teacher in self.teachers.all()])
+        teachers_names = ", ".join([teacher.name for teacher in self.teachers.all()])
         return f"{self.school_class.name} har {self.subject} i {self.classroom}-lokalet med {teachers_names}"
 
 
@@ -125,19 +155,23 @@ class SchoolFee(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
-    school_class = models.ForeignKey(SchoolClass, on_delete=models.CASCADE, related_name='students')  # Tilføj related_name her
-    age_number = models.IntegerField(blank=True, null=True) # klassetrin
+    school_class = models.ForeignKey(
+        SchoolClass, on_delete=models.CASCADE, related_name="students"
+    )  # Tilføj related_name her
+    age_number = models.IntegerField(blank=True, null=True)  # klassetrin
     date_of_birth = models.DateField(blank=True, null=True)
     DEFAULT_FEE_LEVEL = 1
-    school_fee = models.ForeignKey(SchoolFee, on_delete=models.SET_NULL, blank=True, null=True)  # Tilføj denne linje
+    school_fee = models.ForeignKey(
+        SchoolFee, on_delete=models.SET_NULL, blank=True, null=True
+    )  # Tilføj denne linje
 
-    objects = models.Manager() # Den standard manager
+    objects = models.Manager()  # Den standard manager
     in_school = StudentInSchoolManager()
     in_school_class = StudentInSchoolClassManager()
 
     def __str__(self):
         return self.name
-    
+
     @property
     def age(self):
         if self.date_of_birth:
